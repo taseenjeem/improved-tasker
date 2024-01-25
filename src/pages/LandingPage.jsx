f7dde849211f3c9d0ead9b2ca65a5870aa5c6f38
taseenjeem <mdtaseenfuad@gmail.com>
2024-01-25 16:51:01 +0600

    Added main tasker layout

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/page-components/landing-page/banner/Hero";
+import MainTasksLayout from "../components/page-components/landing-page/tasker-layout/MainTasksLayout";
 
 const LandingPage = () => {
   return (
     <main className="bg-[#191D26] font-[Inter] text-white">
       <Hero />
+      <MainTasksLayout />
     </main>
   );
 };
