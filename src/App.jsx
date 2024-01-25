59e19ccfc31136c883f502b31a91c165fee17aa8
taseenjeem <mdtaseenfuad@gmail.com>
2024-01-25 17:06:50 +0600

    Added toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Footer from "./components/global/Footer";
 import Navbar from "./components/global/Navbar";
 import LandingPage from "./pages/LandingPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <main className="bg-[#191D26] font-[Inter] text-white">
-      <Navbar />
-      <LandingPage />
-      <Footer />
-    </main>
+    <>
+      <main className="bg-[#191D26] font-[Inter] text-white">
+        <Navbar />
+        <LandingPage />
+        <Footer />
+      </main>
+      <ToastContainer />
+    </>
   );
 };
 
