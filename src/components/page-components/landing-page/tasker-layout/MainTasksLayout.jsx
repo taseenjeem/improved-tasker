984350f26db4e18976ebc49a8f1af7f137eda53a
taseenjeem <mdtaseenfuad@gmail.com>
2024-01-26 14:19:22 +0600

    removed a prop

--- src/components/page-components/landing-page/tasker-layout/MainTasksLayout.jsx
+++ src/components/page-components/landing-page/tasker-layout/MainTasksLayout.jsx
@@ -14,7 +14,6 @@ const MainTasksLayout = () => {
     <>
       {addTaskModal && (
         <AddTaskModal
-          addTaskModal={addTaskModal}
           setAddTaskModal={setAddTaskModal}
           taskData={taskData}
           setTaskData={setTaskData}
